ee9edfb80df182a39cd487d6732d6f30583845fe
adnaram-m <mmart525@asu.edu>
2026-06-07 13:10:32 -0500

    Story 4: Add guiManageInvitations package with View and Controller stubs
    - Created guiManageInvitations package
    - Create View and Controller files
    - Updated the javadoc for View and Controller files

---
+++ src/guiManageInvitations/ControllerManageInvitations.java
@@ -0,0 +1,25 @@
+package guiManageInvitations;
+
+import database.Database;
+
+/*******
+ * <p> Title: ControllerManageInvitations Class. </p>
+ * 
+ * <p> Description: The Java/FX-based Manage Invitations page.  This class provides the controller
+ * actions for the Manage Invitations page, including loading the invitation list, deleting a selected invitation and 
+ * returning the user to the Admin home page.
+ * 
+ * The class has been written assuming that the View or the Model are the only class methods that
+ * can invoke these methods.  This is why each has been declared at "protected".  Do not change any
+ * of these methods to public.</p>
+ * 
+ * <p> Copyright: Maranda Martinez © 2026 </p>
+ * 
+ * @author Maranda Martinez
+ * 
+ * @version 1.00		2026-06-07 Initial version
+ */
+
+public class ControllerManageInvitations{
+	
+}
---
+++ src/guiManageInvitations/ViewManageInvitations.java
@@ -0,0 +1,31 @@
+package guiManageInvitations;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import database.Database;
+import entityClasses.User;
+import javafx.scene.control.ListView;
+
+/*******
+ * <p> Title: ViewManageInvitations Class. </p>
+ *
+ * <p> Description: The Java/FX-based page for managing outstanding invitations.
+ * Allows the admin to view all pending invitations and manually delete any that
+ * are no longer needed.</p>
+ *
+ * <p> Copyright: Maranda Martinez © 2026 </p>
+ *
+ * @author Maranda Martinez
+ *
+ * @version 1.00		2026-06-07 Initial version
+ *
+ */
+
+public class ViewManageInvitations {
+	
+}
