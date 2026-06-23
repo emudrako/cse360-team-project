6112703cfd0a2f1f6837451c9141b0f53498b09b
adnaram-m <mmart525@asu.edu>
2026-06-22 22:23:34 -0500

    Create guiMyPosts package with corresponding classes
    -Created ViewMyPosts class
    -Created ControllerMyPosts class
    -Created ModelMyPosts class

---
+++ src/guiMyPosts/ControllerMyPosts.java
@@ -0,0 +1,10 @@
+package guiMyPosts;
+
+public class ControllerMyPosts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/guiMyPosts/ModelMyPosts.java
@@ -0,0 +1,5 @@
+package guiMyPosts;
+
+public class ModelMyPosts {
+
+}
